8db6c53a4984e5817e9d0b9e68b20dbd57322ec6
davqasd <david.home@mail.ru>
2021-11-28 22:25:34 +0300

    add activeadmin template

---
+++ activeadmin/template.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'find'
+
+FILES_PATH = "#{File.dirname(__FILE__)}/project/"
+
+def replace_default_db
+  filename = "#{FILES_PATH}/config/database.yml.example"
+  data = File.read(filename)
+             .sub('ENV[\'POSTGRES_DB\']', "ENV['POSTGRES_DB'] || '#{app_name}_development'")
+             .sub('ENV[\'TEST_POSTGRES_DB\']', "ENV['POSTGRES_DB'] || '#{app_name}_test'")
+
+  file 'config/database.yml', data, force: true
+end
+
+def replace_project_files
+  Find.find(FILES_PATH) do |filename|
+    next unless File.file?(filename)
+
+    file filename.sub(FILES_PATH, ''), File.read(filename), force: true
+  end
+end
+
+def init
+  replace_project_files
+
+  run 'bundle install'
+
+  replace_default_db
+  generate 'active_admin:install', 'User'
+
+  run 'rails db:create db:migrate'
+end
+
+init
+
+after_bundle do
+  run 'rubocop -A'
+  run 'rspec'
+
+  git :init
+
+  rails_command 'server'
+end
