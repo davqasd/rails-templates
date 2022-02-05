d50e662c7d1f9a745e52669561dabe1df5172ed0
davqasd <david.home@mail.ru>
2022-02-05 22:23:24 +0300

    add api only template

---
+++ api_only/project/config/routes.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  namespace :api, constraints: { format: 'json' } do
+    # TODO
+  end
+end
---
+++ api_only/template.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+=begin
+rails new -d postgresql --skip-action-cable --skip-turbolinks --skip-test --api \
+  -m rails_templates/api_only/template.rb app_name
+=end
+
+require 'find'
+
+FILES_PATH = "#{File.dirname(__FILE__)}/project/"
+
+def replace_default_db
+  filename = "#{FILES_PATH}/config/database.yml.example"
+  data = File.read(filename)
+             .sub('ENV[\'POSTGRES_DB\']', "ENV['POSTGRES_DB'] || '#{app_name}_development'")
+             .sub('ENV[\'TEST_POSTGRES_DB\']', "ENV['POSTGRES_DB'] || '#{app_name}_test'")
+
+  file 'config/database.yml', data, force: true
+end
+
+def replace_project_files
+  Find.find(FILES_PATH) do |filename|
+    next unless File.file?(filename)
+
+    file filename.sub(FILES_PATH, ''), File.read(filename), force: true
+  end
+end
+
+def init
+  replace_project_files
+
+  run 'bundle install'
+
+  replace_default_db
+  run 'rails db:create db:migrate'
+end
+
+init
+
+after_bundle do
+  run 'rubocop -A'
+  run 'rspec'
+
+  git :init
+
+  rails_command 'server'
+end
